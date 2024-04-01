88e663495c51b3ee25a94f60e4c34b7c0b3de5f3
SeongYoon <namo4862@gmail.com>
2024-04-01 12:42:06 +0900

    fix: App.tsx 컴포넌트 관련 주석 및 컴포넌트추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
+// import { Route, Routes } from "react-router-dom";
+// import Home from "./pages/home";
 import React from "react";
+import CustomButton from "./components/custom/Button";
 
 function App() {
+  const buttonList = [
+    {
+      text: "asdf",
+    },
+    {
+      text: "1234",
+    },
+  ];
+  const spreadButton = {
+    width: 390,
+    height: 390,
+  };
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/" />
-    </Routes>
+    <>
+      {/* Array.map()을 이용해 Component 반복 */}
+      {buttonList.map((items, index) => {
+        return <CustomButton key={index}>{items.text}</CustomButton>;
+      })}
+      {/* 스프레드연산자를 이용해 할당 */}
+      <CustomButton {...spreadButton} />
+    </>
+    // <Routes>
+    //   <Route path="/" element={<Home />} />
+    //   <Route path="/" />
+    // </Routes>
   );
 }
 
