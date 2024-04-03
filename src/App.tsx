9e41c70f2362383f7a7202b1a6dbb38dd39700f4
SeongYoon <namo4862@gmail.com>
2024-04-03 16:32:10 +0900

    fix: 파일명 재구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,7 @@
-import CustomButton from "./components/custom/Button";
-import CustomInput from "./components/custom/Input";
-
 function App() {
-  const buttonList = [
-    {
-      text: "asdf",
-    },
-    {
-      text: "1234",
-    },
-  ];
-  const spreadButton = {
-    width: 390,
-    height: 390,
-  };
-  const inputProps: {
-    type: "text" | "password" | "email";
-    placeholder?: string;
-  } = {
-    type: "text",
-    placeholder: "이것은 Emotion의 styled를 이용해 만든 CustomInput입니다.",
-  };
   return (
     <>
-      {/* Array.map()을 이용해 Component 반복 */}
-      {buttonList.map((items, index) => {
-        return <CustomButton key={index}>{items.text}</CustomButton>;
-      })}
-      {/* 스프레드연산자를 이용해 Props 할당 */}
-      <CustomButton {...spreadButton} />
-      <CustomInput {...inputProps} />
+      <h1>React Study!</h1>
     </>
   );
 }
--- src/pages/home.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const home = () => {
-  return <>g2</>;
-};
-
-export default home;
